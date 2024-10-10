dcaccabfb40540d64f6a550bf0a56cac57be823d
Erin Mikail Staples <54031531+erinmikailstaples@users.noreply.github.com>
2024-10-10 19:00:15 -0400

    update page files in astro

--- ghost-theme/astro.config.mjs
+++ ghost-theme/astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  outDir: '../ghost-theme/content/themes/erin-mikail-theme',
+  outDir: '../ghost-erin-mikail/content/themes/erin-mikail-theme',
   build: {
     format: 'file'
   }
